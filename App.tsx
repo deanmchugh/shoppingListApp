6d0d2e03bcbd6f916e93aca6b2fef6af1b34e7d7
Dean McHugh <dean.mchugh1988@gmail.com>
2020-06-24 20:29:27 +1000

    app and header

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Header from './components/Header'
 
-export default function App() {
+const App = () => {
+  const [items, setItems] = useState([
+    {id: 1, text: 'Milk'},
+    {id: 2, text: 'Bread'},
+    {id: 3, text: 'Apple'},
+    {id: 4, text: 'Soap'},
+  ])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header title='Shopping List' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }
+})
+
+export default App
